e179c70c7d4e921b8089f2d8c0f8c58fa723291b
Mariaclarapcampos <mariaclara.campos@ges.inatel.br>
2025-08-26 14:07:37 -0300

    Add files via upload

---
+++ Ex_cantina/src/Cantina.java
@@ -0,0 +1,21 @@
+public class Cantina{
+    String nome;
+    Salgado salgados[] = new Salgado[3];
+
+    void addSalgado(Salgado novoSalgado) {
+        for (int i = 0; i < salgados.length; i++) {
+            if(salgados[i] == null){
+                salgados[i] = novoSalgado;
+                break;
+            }
+           break;
+        }
+    }
+        void mostraInfo() {
+            for (Salgado salgado : salgados){
+                if(salgado != null){
+                    System.out.println(salgado.nome);
+                }
+            }
+        }
+}
